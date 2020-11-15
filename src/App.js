1a76c50caad0501cf38d9a66f4a12786016e75fb
Bryan Chiu <chiub1@mcmaster.ca>
2020-11-15 00:35:59 -0500

    First carpaal test commit for heroku

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col, ButtonGroup, ToggleButton, Table} from 'react-bootstrap';
+import {HashRouter as Router, Route, Link, NavLink} from 'react-router-dom';
 import './App.css';
+import carpaal_logo from './images/carpaal_logo.png'
+
+
+class CarpaalLogo extends React.Component {
+  render() {
+    return <span><img src={carpaal_logo} className="logo" alt="carpaal logo" /></span>;
+  }
+}
+
+class PageOne extends React.Component {
+  render() {
+  	return (
+      <div>
+        <Container>
+          <Row className="justify-content-md-center">
+            <CarpaalLogo />
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageOne />
   );
 }
 
